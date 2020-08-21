9502b2e7566b9b31fc71a3403c74ad8bfd2e6e6f
AjithArun2001 <ajitharu201@gmail.com>
2020-08-21 15:58:19 +0530

    Updates

---
+++ precache-manifest.433e9173b3526925b5f1978dad3ddb93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade3ec0547402cdfd3b0b92a845e3979",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "e8fc51514890b27e418a",
+    "url": "/expense-tracker/static/css/main.b607d8a9.chunk.css"
+  },
+  {
+    "revision": "8e0f6103853f40bb59e7",
+    "url": "/expense-tracker/static/js/2.d6334375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.d6334375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fc51514890b27e418a",
+    "url": "/expense-tracker/static/js/main.f7dcf0eb.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
